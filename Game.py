1392c8a9ddc0be7963db1869d3cb130a837da586
Luca <luca.95b@live.it>
2021-01-17 21:05:43 +0100

    Lukas forced action

--- Game.py
+++ Game.py
@@ -1,10 +1,17 @@
 import Human, TestPlayer, WoLF, distribution
 from NashQ import NashQ
 import numpy as np
+import random
 import nashpy as nash
-# METHOD = "WoLF"
-METHOD = "NashQ"
-REPEATED_ACTION = True
+METHOD = "WoLF"
+# METHOD = "NashQ"
+
+# REPEATED_ACTION_TYPE = 1 #No repeated action pairs are applied
+# REPEATED_ACTION_TYPE = 2 #Repeated action pairs applied n times, where n is a value coming from poisson distribution
+REPEATED_ACTION_TYPE = 3  # specific move repeated with lambda probability
+if REPEATED_ACTION_TYPE == 3:
+    LAMBDA_VALUE = 0.3
+    FORCED_ACTION = [0,0]
 
 def updateNash(player, actions, nash_actions, rewards, tick):
     player.update(actions[player.player], actions[1 - player.player], nash_actions, 0, rewards)
@@ -19,7 +26,7 @@ class Game:
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
         self.game = [[self.topLeft, self.topRight], [self.bottomLeft, self.bottomRight]]
-        print("game", self.game)
+
         self.distr = distribution.distribution()
         if METHOD == 'WoLF':
             self.playerOne = WoLF.WoLF(0, 0.5, 0.9, 0.05, 0.1, 1, 2)
@@ -49,23 +56,30 @@ class Game:
 
     def loop(self, onePick, twoPick):
         updateCount = 0
+        action_forced = 0
         players = [self.playerOne, self.playerTwo]
         actions = []
         nash_actions = []
         rewards = self.getMovesReward(onePick, twoPick)
         while(self.tick < 10000):
 
-            if not REPEATED_ACTION:  # activate or deactivate repeated action pairs
+            if not REPEATED_ACTION_TYPE == 2:  # activate or deactivate repeated action pairs
                 self.nextMove = self.tick
 
             if METHOD == 'WoLF':
                 if self.tick == self.nextMove:
                     updateCount += 1
-                    onePick = self.playerOne.getMove(self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), [onePick, twoPick], self.tick)
-                    twoPick = self.playerTwo.getMove(self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()), [onePick, twoPick], self.tick)
+                    if REPEATED_ACTION_TYPE == 3 and random.random() < LAMBDA_VALUE:
+                        action_forced += 1
+                        onePick, twoPick = FORCED_ACTION
+                        print("action forced", FORCED_ACTION)
+                    else:
+                        onePick = self.playerOne.getMove(self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), [onePick, twoPick], self.tick)
+                        twoPick = self.playerTwo.getMove(self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()), [onePick, twoPick], self.tick)
                     self.nextMove = self.tick + self.distr.getNext()
                     print("Player 0: ", "Pick: ", onePick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), "Tick: ", self.tick)
                     print("Player 1: ", "Pick: ", twoPick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()))
+
                 self.playerOne.update(onePick, 0, self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()))
                 self.playerTwo.update(twoPick, 0, self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()))
 
@@ -75,8 +89,13 @@ class Game:
                     updateCount += 1
                     actions = []
                     #choose action
-                    for player in players:
-                        actions.append(player.select_action(self.tick))
+                    if REPEATED_ACTION_TYPE == 3 and random.random() < LAMBDA_VALUE:
+                        action_forced += 1
+                        actions = FORCED_ACTION
+                        print("action repeated", FORCED_ACTION)
+                    else:
+                        for player in players:
+                            actions.append(player.select_action(self.tick))
                     self.nextMove = self.tick + self.distr.getNext()
                     onePick, twoPick = actions
                     print("Player 0: ", "Pick: ", onePick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), "Tick: ", self.tick)
@@ -98,6 +117,7 @@ class Game:
         print("Player 0: ","Pick: ", onePick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), "Tick: " ,self.tick)
         print("Player 1: ","Pick: ", twoPick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()))
         print(f"Number of Selections: {updateCount}")
+        print(f"Number of forced actions: {action_forced}")
         print("player 1 played:", int(self.countMoves[0][0]), int(self.countMoves[0][1]))
         print("player 2 played:", int(self.countMoves[1][0]), int(self.countMoves[1][1]))
         # self.playerOne.printpi()
@@ -105,7 +125,8 @@ class Game:
         #onePick*2+twoPick
 
 def main():
-    # newGame = Game([2,2], [5,0], [0,5], [4,4]) #prisoner dilemma
+    # newGame = Game([5,5], [6,0],
+    #                [0,6], [1,1]) #prisoner dilemma
     newGame = Game([1,-1], [-1,1], [-1,1], [1,-1]) #matching pennies
 
     # print(newGame)
