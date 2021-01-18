35c6a46fc24521d0f71d1894787bb58241be060a
Luca <luca.95b@live.it>
2021-01-18 01:33:25 +0100

    nash-Q learnign correction and PI implementation

--- Game.py
+++ Game.py
@@ -1,21 +1,28 @@
 import Human, TestPlayer, WoLF, distribution
+import distribution
 from NashQ import NashQ
 import numpy as np
 import random
 import nashpy as nash
-METHOD = "WoLF"
-# METHOD = "NashQ"
-
-# REPEATED_ACTION_TYPE = 1 #No repeated action pairs are applied
-# REPEATED_ACTION_TYPE = 2 #Repeated action pairs applied n times, where n is a value coming from poisson distribution
-REPEATED_ACTION_TYPE = 3  # specific move repeated with lambda probability
+# METHOD = "WoLF"
+METHOD = "NashQ"
+REPEATED_ACTION_TYPE = 2
+# if REPEATED_ACTION_TYPE = 1 no repeated action pairs are applied
+# if REPEATED_ACTION_TYPE = 2 every action is forced to be repeated n times, n is the output value of poisson
+#                             distribution. note: a new 'n' is calculated every time.
+# if REPEATED_ACTION_TYPE = 3  a specific move is forced to happen with a lambda probability
 if REPEATED_ACTION_TYPE == 3:
     LAMBDA_VALUE = 0.3
-    FORCED_ACTION = [0,0]
+    FORCED_ACTION = [1,1]
 
-def updateNash(player, actions, nash_actions, rewards, tick):
-    player.update(actions[player.player], actions[1 - player.player], nash_actions, 0, rewards)
+def updateNash(player, actions, nash_actions, nash_actions_opp, rewards, tick):
+    player.Q[0, 0, actions[0], actions[1]] = player.update(actions[player.player], actions[1 - player.player], nash_actions,     player.Q[0], rewards[0], 0)
+    player.Q[1, 0, actions[1], actions[0]] = player.update(actions[1 - player.player], actions[player.player], nash_actions_opp, player.Q[1], rewards[1], 0)
     player.epsilon = 1 / tick
+    # player.epsilon *= player.epsilon * 0.999
+    # player.epsilon *=  0.999
+    if player.epsilon < 0.01:
+        player.epsilon = 0.01
     return player
 
 class Game:
@@ -32,8 +39,8 @@ class Game:
             self.playerOne = WoLF.WoLF(0, 0.5, 0.9, 0.05, 0.1, 1, 2)
             self.playerTwo = WoLF.WoLF(1, 0.5, 0.9, 0.05, 0.1, 1, 2)
         else:
-            self.playerOne = NashQ(player=0, alpha=0.5, gamma=0.9, epsilon=0.5, num_states=1, num_actions=2)
-            self.playerTwo = NashQ(player=1, alpha=0.5, gamma=0.9, epsilon=0.5, num_states=1, num_actions=2)
+            self.playerOne = NashQ(player=0, alpha=0.5, gamma=0.9, epsilon=1, num_states=1, num_actions=2)
+            self.playerTwo = NashQ(player=1, alpha=0.5, gamma=0.9, epsilon=1, num_states=1, num_actions=2)
         self.tick = 0
         self.countMoves = np.zeros((2, 2)) # (n_player, num_actions)
 
@@ -60,8 +67,9 @@ class Game:
         players = [self.playerOne, self.playerTwo]
         actions = []
         nash_actions = []
+        nash_actions_opp = []
         rewards = self.getMovesReward(onePick, twoPick)
-        while(self.tick < 10000):
+        while(self.tick < 5000):
 
             if not REPEATED_ACTION_TYPE == 2:  # activate or deactivate repeated action pairs
                 self.nextMove = self.tick
@@ -96,19 +104,31 @@ class Game:
                     else:
                         for player in players:
                             actions.append(player.select_action(self.tick))
-                    self.nextMove = self.tick + self.distr.getNext()
                     onePick, twoPick = actions
+                    self.nextMove = self.tick + self.distr.getNext()
+
                     print("Player 0: ", "Pick: ", onePick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerOne.getPlayer()), "Tick: ", self.tick)
                     print("Player 1: ", "Pick: ", twoPick, "Reward:", self.getMoveReward(onePick, twoPick, self.playerTwo.getPlayer()))
                     rewards = self.getMovesReward(onePick,twoPick)
                     nash_actions = []
+                    nash_actions_opp = []
                     # search nash strategy
-                    for i, player in enumerate(players):
-                        nash_actions.append(player.nash_action(0))
+                    # for i, player in enumerate(players):
+                    #     pi, pi_opp = player.compute_pi(0)
+                    #     print(f"pi of :{i} {pi}")
+                    #     nash_actions.append(player.nash_action(0, pi, pi_opp, player.Q[0]))
+                    #     nash_actions_opp.append(player.nash_action(0, pi_opp, pi, player.Q[1]))
+
+                nash_actions = []
+                nash_actions_opp = []
                 players_update = []
                 #observe and update
-                for player in players:
-                    players_update.append(updateNash(player, actions, nash_actions, rewards, self.tick))
+                for i, player in enumerate(players):
+                    pi, pi_opp = player.compute_pi(0)
+                    print(f"pi of :{i} {pi}")
+                    nash_actions = player.nash_action(0, pi, pi_opp, player.Q[0])
+                    nash_actions_opp = player.nash_action(0, pi_opp, pi, player.Q[1])
+                    players_update.append(updateNash(player, actions, nash_actions, nash_actions_opp, rewards, self.tick))
                 players = players_update
 
             self.countMoves[0, onePick] += 1
@@ -125,9 +145,9 @@ class Game:
         #onePick*2+twoPick
 
 def main():
-    # newGame = Game([5,5], [6,0],
-    #                [0,6], [1,1]) #prisoner dilemma
-    newGame = Game([1,-1], [-1,1], [-1,1], [1,-1]) #matching pennies
+    newGame = Game([5,5], [6,0],
+                   [0,6], [1,1]) #prisoner dilemma
+    # newGame = Game([1,-1], [-1,1], [-1,1], [1,-1]) #matching pennies
 
     # print(newGame)
     # print(newGame.getMoveValues(0, 1))
--- NashQ.py
+++ NashQ.py
@@ -1,7 +1,6 @@
 import random
 import numpy as np
 import nashpy as nash
-from collections import defaultdict
 
 class NashQ:
 
@@ -25,6 +24,7 @@ class NashQ:
     """calculate actions a^i_t"""
     def select_action(self,tick):
         state = self.last_state
+        # pi, pi_o = self.compute_pi(state)
         if random.random() < self.epsilon:
             choice = np.random.choice(np.arange(self.num_actions), 1)[0]
         else:
@@ -38,62 +38,121 @@ class NashQ:
             eqs = list(rps.support_enumeration())
             eqs_0 = eqs[0][0].tolist()
             choice = np.random.choice(np.arange(len(eqs_0)), p=eqs_0)
+
+            # choice = np.random.choice(np.flatnonzero(pi == pi.max()))
         return choice
 
     #get nash Equilibrium
-    def nash_action(self,state):
-        A = self.Q[0, state]
-        B = self.Q[1, state]
-        rps = nash.Game(A, B)
-        # print(rps)
-        choices = [0, 0]
-        eqs = list(rps.support_enumeration())
-        nash_length = len(eqs)
-        try:
-            # nash_choice = np.random.choice(np.arange(nash_length), 1)[0]  #Random choice of Nash-equilibrium
-            nash_choice = 0
-            eqs_0 = eqs[nash_choice][0].tolist()
-            # print("Eqssss", eqs_0)
-            choices[0] = np.random.choice(np.arange(len(eqs_0)), p=eqs_0)
-            eqs_1 = eqs[nash_choice][1].tolist()
-            # print("Eqssss1", eqs_1)
-            choices[1] = np.random.choice(np.arange(len(eqs_1)), p=eqs_1)
-            # return [eqs_0, eqs_1]
-            # if choices[0]==1 or choices[1]==1:
-            return choices
-        except IndexError:
-            return [None, None]
+    def nash_action(self, state, pi, pi_o,q):
+        # A = self.Q[0, state]
+        # B = self.Q[1, state]
+        # rps = nash.Game(A, B)
+        # # print(rps)
+        # choices = [0, 0]
+        # eqs = list(rps.support_enumeration())
+        # nash_length = len(eqs)
+        # try:
+        #     # nash_choice = np.random.choice(np.arange(nash_length), 1)[0]  #Random choice of Nash-equilibrium
+        #     nash_choice = 0
+        #     eqs_0 = eqs[nash_choice][0].tolist()
+        #     # print("Eqssss", eqs_0)
+        #     choices[0] = np.random.choice(np.arange(len(eqs_0)), p=eqs_0)
+        #     eqs_1 = eqs[nash_choice][1].tolist()
+        #     # print("Eqssss1", eqs_1)
+        #     choices[1] = np.random.choice(np.arange(len(eqs_1)), p=eqs_1)
+        #     # return [eqs_0, eqs_1]
+        #     # if choices[0]==1 or choices[1]==1:
+        #     return choices
+        # except IndexError:
+        #     return [None, None]
+        nashq = 0
+        for action1 in range(self.num_actions):
+            for action2 in range(self.num_actions):
+                nashq += pi[action1] * pi_o[action2] * \
+                         q[state][(action1, action2)]
+        # print(nashq)
+        return nashq
 
-    def update(self, own, other,nash_actions, state, rewards):
+    def update(self, own, other, nash_action, q, reward, state):
         self.history_alpha[state, own, other] += 1
-        nash_actions_local = nash_actions[self.player]
-        V = np.zeros(2)
-        if None not in nash_actions_local:
-            V[0] = self.Q[0, state, nash_actions_local[0], nash_actions_local[1]]
-            V[1] = self.Q[1, state, nash_actions_local[1], nash_actions_local[0]]
-            # print("V",V)
-            # for i in range(len(nash_actions_local)):
-            #     for j in range(len(nash_actions_local)):# it's probabilities now
-            #         V[0] += self.Q[0, state, i, j]*nash_actions[0][i]*nash_actions[1][j]
-            # for i in range(len(nash_actions_local)):
-            #     for j in range(len(nash_actions_local)):# it's probabilities now
-            #         V[1] += self.Q[1, state, j, i]*nash_actions[0][i]*nash_actions[1][j]
-        else:
-            V[0] = self.val(state, 0)
-            V[1] = self.val(state, 1)
+        q_old = q[0, own, other]
+        self.alpha = self.base_alpha / (self.history_alpha[state, own, other])
+        # self.alpha = 1 / (self.history_alpha[state, own, other])
+        if self.alpha < 0.001:
+            self.alpha = 0.001
+        updated_q = q_old + \
+                    (self.alpha * (reward + (self.gamma * nash_action) - q_old))
 
-        alpha = self.base_alpha / (self.history_alpha[state, own, other])
+        return updated_q
 
-        #  Qit+1(s;a1; : : : ;an) = (1−alpha) * Qit(s;a1, a2)+         alpha * [rit     +          gamma * (NashQit(s')]
-        self.Q[0, state, own, other] = (1 - alpha) * self.Q[0, state, own, other] + alpha * (rewards[self.player] + self.gamma * V[0])
-        self.Q[1, state, other, own] = (1 - alpha) * self.Q[1, state, other, own] + alpha * (rewards[1 - self.player] + self.gamma * V[1])
+        # self.history_alpha[state, own, other] += 1
+        # nash_actions_local = nash_actions[self.player]
+        # V = np.zeros(2)
+        # if None not in nash_actions_local:
+        #     V[0] = self.Q[0, state, nash_actions_local[0], nash_actions_local[1]]
+        #     V[1] = self.Q[1, state, nash_actions_local[1], nash_actions_local[0]]
+        #     # print("V",V)
+        #     # for i in range(len(nash_actions_local)):
+        #     #     for j in range(len(nash_actions_local)):# it's probabilities now
+        #     #         V[0] += self.Q[0, state, i, j]*nash_actions[0][i]*nash_actions[1][j]
+        #     # for i in range(len(nash_actions_local)):
+        #     #     for j in range(len(nash_actions_local)):# it's probabilities now
+        #     #         V[1] += self.Q[1, state, j, i]*nash_actions[0][i]*nash_actions[1][j]
+        # else:
+        #     V[0] = self.val(state, 0)
+        #     V[1] = self.val(state, 1)
+        #
+        # alpha = self.base_alpha / (self.history_alpha[state, own, other])
+        #
+        # #  Qit+1(s;a1; : : : ;an) = (1−alpha) * Qit(s;a1, a2)+         alpha * [rit     +          gamma * (NashQit(s')]
+        # self.Q[0, state, own, other] = (1 - alpha) * self.Q[0, state, own, other] + alpha * (rewards[self.player] + self.gamma * V[0])
+        # self.Q[1, state, other, own] = (1 - alpha) * self.Q[1, state, other, own] + alpha * (rewards[1 - self.player] + self.gamma * V[1])
 
-            # except ValueError:
-            #     print(V)
-            #     print(nash_actions_local)
-            #     print(nash_actions)
 
+    def compute_pi(self, state):
+        """
+            compute pi (nash)
+        """
+        # q_1, q_2 = [], []
+        # for action1 in range(self.num_actions):
+        #     row_q_1, row_q_2 = [], []
+        #     for action2 in range(self.num_actions):
+        #         joint_action = (action1, action2)
+        #         row_q_1.append(self.Q[0,state,action1,action2])
+        #         row_q_2.append(self.Q[1,state,action1,action2])
+        #
+        #     q_1.append(row_q_1)
+        #     q_2.append(row_q_2)
 
+        q_1 = self.Q[0, state]
+        q_2 = self.Q[1, state]
+        game = nash.Game(q_1, q_2)
+        equilibria = game.support_enumeration()
+        # equilibria = game.lemke_howson_enumeration()
+        # equilibria = game.vertex_enumeration()
+        pi_list = list(equilibria)
+
+        pi = None
+        for _pi in pi_list:
+            if _pi[0].shape == (self.num_actions, ) and _pi[1].shape == (
+                    self.num_actions, ):
+                if any(
+                    np.isnan(
+                        _pi[0])) is False and any(
+                    np.isnan(
+                        _pi[1])) is False:
+                    pi = _pi
+                    break
+
+        if pi is None:
+            pi1 = np.repeat(
+                1.0 / self.num_actions, self.num_actions)
+            pi2 = np.repeat(
+                1.0 / self.num_actions, self.num_actions)
+
+            pi = (pi1, pi2)
+
+        return pi[0], pi[1]
 
 #
     def getPlayer(self):
